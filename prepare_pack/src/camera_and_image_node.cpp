ffc61177a4cbcb05a3cbba8044534fe2e052611e
tsuchida <tsuchida.shinya413@mail.kyutech.jp>
2021-06-13 11:24:04 +0900

    fef

--- prepare_pack/src/camera_and_image_node.cpp
+++
@@ -1,9 +0,0 @@
-#include <prepare_pack/camera_and_image.hpp>
-
-int main(int argc, char** argv)
-{
-    ros::init(argc, argv, "int");
-    camera_and_image cam_i;
-    ros::spin();
-    return 0;
-}
